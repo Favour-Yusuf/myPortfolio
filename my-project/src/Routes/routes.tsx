e8fd6ae604ee0636e67d178daf653ca85a52255d
Favour-Yusuf <favouryusuf45@gmail.com>
2023-05-30 15:59:40 +0100

    re-installed

--- my-project/src/Routes/routes.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { HomeLayout } from "../Components";
-
-export const element = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeLayout />,
-  },
-]);
