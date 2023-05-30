e4bbd2d5cb75b86a5ca93ba3ff083d1b5034dd12
Favour-Yusuf <favouryusuf45@gmail.com>
2023-05-30 16:44:42 +0100

    started header

--- my-project/src/Components/Static/Header.tsx
+++ my-project/src/Components/Static/Header.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full h-16 bg-red-400 flex items-center justify-between">
+      <div>Logo</div>
+      <div>Navbar</div>
+      <button>Button</button>
+    </div>
+  );
 };
 
 export default Header;
